1ceb060ffa47d9213d175f17e833a8819a6cf19d
Artem <dzaparidzeartem@gmail.com>
2026-01-27 01:18:12 +0500

    feat: добавлена фильтрация в GetObjectsList
    Добавлена фильтрация по типу события, по дате ОТ и ДО. Фильтры работают как вместе так и по отдельности.

--- internal/objects/get_objects_list.go
+++ internal/objects/get_objects_list.go
@@ -6,7 +6,15 @@ import (
 
 	"github.com/airsss993/histproject-backend/pkg/db"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
+
+// GetObjectsListReq - структура запроса для получения списка объектов с фильтрацией
+type GetObjectsListReq struct {
+	EventTypeIDs []int  `json:"eventTypeIds" binding:"omitempty,dive,gt=0"`
+	DateFrom     string `json:"dateFrom" binding:"omitempty,datetime=2006-01-02"`
+	DateTo       string `json:"dateTo" binding:"omitempty,datetime=2006-01-02"`
+}
 
 // GetObjectsListResp - структура ответа для получения списка объектов
 type GetObjectsListResp struct {
@@ -28,15 +36,21 @@ type GetObjectsListResp struct {
 // @Success		200	{objects} ObjectInfo
 // @Router		/objects/get-objects-list [get]
 func GetObjectsList(c *gin.Context) {
-	// Инициализируем структуру ответа
-	//out := GetObjectsListResp{
-	//	Objects: []map[string]string{},
-	//}
+	// Инициализируем структуру запроса для того чтобы заполнить её данными
+	var req GetObjectsListReq
 
-	resp := GetObjectsListResp{}
+	if c.Request.ContentLength > 0 {
+		err := c.ShouldBindJSON(&req)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "Ошибка парсинга JSON: " + err.Error(),
+			})
+			return
+		}
+	}
 
-	// Получаем данные об одном объекте по его ID
-	objects, err := getObjectsListFromDB()
+	// Получаем список объектов с фильтрами
+	objects, err := getObjectsListFromDB(req.EventTypeIDs, req.DateFrom, req.DateTo)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Ошибка получения данных из БД: " + err.Error(),
@@ -44,17 +58,13 @@ func GetObjectsList(c *gin.Context) {
 		return
 	}
 
-	resp.Objects = objects
-
 	// Отправляем успешный ответ
-	c.JSON(http.StatusOK, gin.H{
-		"objects": resp.Objects,
-	})
+	c.JSON(http.StatusOK, GetObjectsListResp{Objects: objects})
 
 }
 
 // getObjectsListFromDB - функция для получения информации о всех объектах из БД.
-func getObjectsListFromDB() ([]ObjectInfo, error) {
+func getObjectsListFromDB(eventTypesIds []int, dateFrom, dateTo string) ([]ObjectInfo, error) {
 	var objectsInfo []ObjectInfo
 
 	query := `
@@ -68,9 +78,45 @@ func getObjectsListFromDB() ([]ObjectInfo, error) {
 		FROM
 		    histproject.objects`
 
-	err := db.DB.Select(&objectsInfo, query)
+	var args []interface{}
+	argPos := 1
+
+	// Если пришли типы, то добавляем фильтрацию по ним
+	if len(eventTypesIds) > 0 {
+		query += fmt.Sprintf(" WHERE event_type_id = ANY($%d)", argPos)
+		args = append(args, pq.Array(eventTypesIds))
+		argPos++
+	}
+
+	// Если пришла дата начала, то добавляем
+	if dateFrom != "" {
+		if argPos > 1 {
+			query += fmt.Sprintf(" AND event_date >= $%d", argPos)
+		} else {
+			query += fmt.Sprintf(" WHERE event_date >= $%d", argPos)
+		}
+		args = append(args, dateFrom)
+		argPos++
+	}
+
+	// Если пришла дата конца, то добавляем
+	if dateTo != "" {
+		if argPos > 1 {
+			query += fmt.Sprintf(" AND event_date <= $%d", argPos)
+		} else {
+			query += fmt.Sprintf(" WHERE event_date <= $%d", argPos)
+		}
+		args = append(args, dateTo)
+		argPos++
+	}
+
+	err := db.DB.Select(&objectsInfo, query, args...)
 	if err != nil {
 		return nil, fmt.Errorf("ошибка получения объектов с БД: %w", err)
+	}
+
+	if objectsInfo == nil {
+		return []ObjectInfo{}, nil
 	}
 
 	return objectsInfo, nil
--- internal/router/router.go
+++ internal/router/router.go
@@ -39,7 +39,7 @@ func InitRoutes(r *gin.Engine) {
 		// Эндпоинт для получения информации об одном объекте
 		public.GET("objects/get-object-data/:id", objects.GetObjectData)
 		// Эндпоинт для получения всех объектов с их информацией
-		public.GET("objects/get-objects-list", objects.GetObjectsList)
+		public.POST("objects/get-objects-list", objects.GetObjectsList)
 		// Эндпоинт для получения всех типов событий с их информацией
 		public.GET("objects/get-event-types-list", objects.GetEventTypesList)
 	}
