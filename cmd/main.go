d9a9609334df827e3fee4d036e0f581afbc5f28a
Artem <189112526+airsss993@users.noreply.github.com>
2026-03-04 22:24:05 +0500

    fix: удаление импорта Swagger-документации из main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	_ "github.com/airsss993/histproject-backend/docs"
 	"github.com/airsss993/histproject-backend/internal/app"
 )
 
-// @title Документация API исторической платформы
-// @version 1.0
-// @description Серверное REST API исторической платформы, разработанное на Go с использованием Gin.
-// @BasePath /api
 func main() {
 	app.Run()
 }
